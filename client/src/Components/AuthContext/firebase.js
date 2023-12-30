7501ebc1ee30e56cf587bc963f9088fdc133bbf8
Federico Irarrazaval <federico.irarrazaval@itglobers.com>
2023-12-29 22:59:41 -0300

    fix 3

--- client/src/Components/AuthContext/firebase.js
+++ client/src/Components/AuthContext/firebase.js
@@ -4,13 +4,13 @@ import { getAuth } from 'firebase/auth';
 import { initializeFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyA71Ptkw2pB43zBfOlmWPWXTmIftrJOMbg',
-  authDomain: 'first-try-6e607.firebaseapp.com',
-  databaseURL: 'https://first-try-6e607-default-rtdb.firebaseio.com',
-  projectId: 'first-try-6e607',
-  storageBucket: 'first-try-6e607.appspot.com',
-  messagingSenderId: '848679521149',
-  appId: '1:848679521149:web:2309eba141174fa2d25a14',
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
 };
 
 const app = initializeApp(firebaseConfig);
