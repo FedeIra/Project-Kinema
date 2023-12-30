83ee6c1cf67432d833c48243dc62ef7e2a2d6cc6
Federico Irarrazaval <federico.irarrazaval@itglobers.com>
2023-12-29 23:09:01 -0300

    example

--- client/src/Components/AuthContext/firebase.js
+++ client/src/Components/AuthContext/firebase.js
@@ -5,12 +5,12 @@ import { initializeFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyA71Ptkw2pB43zBfOlmWPWXTmIftrJOMbg',
-  authDomain: '',
-  databaseURL: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: '',
+  authDomain: 'first-try-6e607.firebaseapp.com',
+  databaseURL: 'https://first-try-6e607-default-rtdb.firebaseio.com',
+  projectId: 'first-try-6e607',
+  storageBucket: 'first-try-6e607.appspot.com',
+  messagingSenderId: '848679521149',
+  appId: '1:848679521149:web:2309eba141174fa2d25a14',
 };
 
 const app = initializeApp(firebaseConfig);
