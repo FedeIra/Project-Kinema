3e62819b20b9d79ea5f6b0d3d85a78e6cd8e7769
Federico Irarrazaval <federico.irarrazaval@itglobers.com>
2023-12-29 22:50:36 -0300

    firebase settings

--- client/src/Components/AuthContext/firebase.js
+++ client/src/Components/AuthContext/firebase.js
@@ -1,17 +1,16 @@
 // Import the functions for SDKs:
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-// import { getFirestore } from 'firebase/firestore';
 import { initializeFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyA71Ptkw2pB43zBfOlmWPWXTmIftrJOMbg',
-  authDomain: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: '',
-  measurementId: '',
+  authDomain: 'first-try-6e607.firebaseapp.com',
+  databaseURL: 'https://first-try-6e607-default-rtdb.firebaseio.com',
+  projectId: 'first-try-6e607',
+  storageBucket: 'first-try-6e607.appspot.com',
+  messagingSenderId: '848679521149',
+  appId: '1:848679521149:web:2309eba141174fa2d25a14',
 };
 
 const app = initializeApp(firebaseConfig);
